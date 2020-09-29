7d62db2e9e657ea48e13cdf0a70d4d822d7f8405
EJ Hammond <ejhammond@hey.com>
2020-09-29 18:34:56 -0400

    Redirect to blog until we have other content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 import { Link } from 'gatsby';
-
+import { Redirect } from '@reach/router';
 import Bio from '../components/bio';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -12,6 +12,7 @@ const Index: React.FC = () => {
       <SEO title="Home" />
       <Bio />
       <Link to="/blog">Blog</Link>
+      <Redirect noThrow to="/blog" />
     </Layout>
   );
 };
