377f559a14fb72676b5380d74cb3623d6bb79164
EJ Hammond <ejhammond@hey.com>
2024-09-30 22:26:23 -0400

    Memo breadcrumbs

--- src/components/site-breadcrumbs.tsx
+++ src/components/site-breadcrumbs.tsx
@@ -4,27 +4,35 @@ import { Breadcrumbs } from '@/ds/breadcrumbs';
 import { Link } from '@/ds/link';
 import { makeHeading } from '@/utils/string';
 import { useSelectedLayoutSegments } from 'next/navigation';
+import { useMemo } from 'react';
 import { Breadcrumb } from 'react-aria-components';
+
 type Item = { id: string; href?: string };
 
 export function SiteBreadcrumbs() {
   const parts = useSelectedLayoutSegments();
-  const previousParts = parts.slice(0, -1);
-  const currentPart = parts.slice(-1)[0];
+  const breadcrumbs = useMemo(() => {
+    const previousParts = parts.slice(0, -1);
+    const currentPart = parts.slice(-1)[0];
 
-  const breadcrumbs: Item[] =
-    currentPart !== undefined ? [{ id: 'home', href: '/' }] : [{ id: 'home' }];
+    const mutableList: Item[] =
+      currentPart !== undefined
+        ? [{ id: 'home', href: '/' }]
+        : [{ id: 'home' }];
 
-  for (let i = 0; i < previousParts.length; ++i) {
-    const part = previousParts[i];
-    const url = '/' + previousParts.slice(0, i + 1).join('/');
+    for (let i = 0; i < previousParts.length; ++i) {
+      const part = previousParts[i];
+      const url = '/' + previousParts.slice(0, i + 1).join('/');
 
-    breadcrumbs.push({ id: part, href: url });
-  }
+      mutableList.push({ id: part, href: url });
+    }
 
-  if (currentPart !== undefined) {
-    breadcrumbs.push({ id: currentPart });
-  }
+    if (currentPart !== undefined) {
+      mutableList.push({ id: currentPart });
+    }
+
+    return mutableList;
+  }, [parts]);
 
   return (
     <Breadcrumbs key={breadcrumbs.join('.')} items={breadcrumbs}>
