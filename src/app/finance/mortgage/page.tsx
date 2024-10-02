172c0b095cbb267fac5132a027eed62b59592a48
EJ Hammond <ejhammond@hey.com>
2024-10-02 09:35:38 -0400

    Mortgage table responsiveness

--- src/app/finance/mortgage/page.tsx
+++ src/app/finance/mortgage/page.tsx
@@ -7,6 +7,17 @@ import { getMortgagePayoff } from 'mortgage';
 import { useState } from 'react';
 import { TableBody } from 'react-aria-components';
 
+import './page.css';
+
+function formatUSD(value: number): string {
+  return Intl.NumberFormat([], {
+    style: 'currency',
+    currencySign: 'standard',
+    currency: 'USD',
+    currencyDisplay: 'symbol',
+  }).format(value);
+}
+
 export default function Mortgage() {
   const [amount, setAmount] = useState(100000);
   const [term, setTerm] = useState(30);
@@ -56,32 +67,28 @@ export default function Mortgage() {
           }}
         />
       </Form>
-      <Table
-        aria-label="Loan amortization schedule"
-        style={{
-          marginBlockStart: 32,
-          fontFamily: 'var(--global-font-monospace)',
-        }}
-      >
-        <TableHeader>
-          <Column isRowHeader>Month</Column>
-          <Column>Principal</Column>
-          <Column>Interest</Column>
-          <Column>Balance</Column>
-        </TableHeader>
-        <TableBody>
-          {Array.from({ length: term * 12 }).map((_, month) => {
-            return (
-              <Row key={month}>
-                <Column>{month}</Column>
-                <Column>${payoff.data[month].principal.toFixed(2)}</Column>
-                <Column>${payoff.data[month].interest.toFixed(2)}</Column>
-                <Column>${payoff.data[month].balance.toFixed(2)}</Column>
-              </Row>
-            );
-          })}
-        </TableBody>
-      </Table>
+      <div id="mortgage-table">
+        <Table aria-label="Loan amortization schedule">
+          <TableHeader>
+            <Column isRowHeader>Month</Column>
+            <Column>Principal</Column>
+            <Column>Interest</Column>
+            <Column>Balance</Column>
+          </TableHeader>
+          <TableBody>
+            {Array.from({ length: term * 12 }).map((_, month) => {
+              return (
+                <Row key={month}>
+                  <Column>{month}</Column>
+                  <Column>{formatUSD(payoff.data[month].principal)}</Column>
+                  <Column>{formatUSD(payoff.data[month].interest)}</Column>
+                  <Column>{formatUSD(payoff.data[month].balance)}</Column>
+                </Row>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
     </>
   );
 }
